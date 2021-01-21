0da5c75da544fa1eac6ca2434304af3142202e3d
Vinicius Alves Rodrigues da Silva <vinicius.silva@yandeh.com.br>
2021-01-21 03:05:16 -0300

    Configurando o banco remoto

--- config/db.js
+++ config/db.js
@@ -1,16 +1,24 @@
 const Sequelize = require('sequelize');
 const pg = require('pg')
 
-//conexão com banco de dados
-const sequelize = new Sequelize('postgres', 'postgres', 'Vinizika231199', {
-    host: 'localhost',
+pg.defaults.ssl = true
+
+//conexão com banco de dados       // DATABASE       // USERNAME                     /// PASSWORD
+const sequelize = new Sequelize('des6i46h71m5v6', 'xphbxexnrfvzxa', 'c2d272a740e5ed27c0c405205af6e363ceb9ccc55a29f88b5066108082c1bbd3', {
+    host: 'ec2-54-159-107-189.compute-1.amazonaws.com', /// se estive rlocal use localhost
     port: 5432,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    dialectOptions: { // remover esse atributo quando estiver rodando local
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+        }
+    },
 })
 
-sequelize.authenticate().then(()=>{
+sequelize.authenticate().then(() => {
     console.log("Conectado com sucesso")
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err)
 })
 
